26caa821c75f67069b2871a618330ed891ffc605
Marzela547 <marcelazelaya547@yahoo.com>
2021-03-01 02:17:41 -0600

    Final esta vez sí

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -1,6 +1,6 @@
 function Form({children}){
   return (
-    <form className="block text-white flex-wrap left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute box-border py-16 px-7 lg:min-w-min">
+    <form className="block text-white flex-wrap left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute box-border py-16 px-7 min-w-max lg:min-w-min">
         <h1 className="border-b-4 border-pink-600 text-4xl mb-12 px-1 py-0 w-28 font-bold">Login</h1>
         {children}
     </form>
