76e9699b77dcbea550b01f7a622abe3c3e395c59
Marzela547 <marcelazelaya547@yahoo.com>
2021-03-01 00:06:38 -0600

    Avance5

--- src/components/Forms/FormField.js
+++ src/components/Forms/FormField.js
@@ -2,7 +2,7 @@ function FormField({ type, id, fname, onChange, caption, value, error, ...attr})
   return (
     <section>
     <label>{caption}</label>
-    <input class=" w-full mb-5 border-b-2 h-10 text-base border-white bg-transparent"
+    <input className="w-full mb-5 border-b-2 h-10 text-base border-white bg-transparent"
         type={type}
         id={id}
         name={fname}
