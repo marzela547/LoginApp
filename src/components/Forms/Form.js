1718ba40eaa497b507cf19562807e8ec0b269e81
Marzela547 <marcelazelaya547@yahoo.com>
2021-03-01 20:31:02 -0600

    Avance 8, esta si, final final

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -1,7 +1,7 @@
 function Form({children}){
   return (
-    <form className="block text-white flex-wrap left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 md:w-3/5 md:h-1/2 md:text-3xl md:py-16 md:px-7 absolute box-border lg:py-16 lg:px-7 lg:min-w-min">
-        <h1 className="border-b-4 border-pink-600 text-2xl mb-3 mt-2 md:border-b-8 md:mb-12 md:text-5xl md:mt-0 lg:text-4xl lg:mb-12 px-1 py-0 lg:w-28 font-bold">Login</h1>
+    <form className="block text-white flex-wrap left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 md:w-3/5 md:h-1/2 md:text-3xl md:py-16 md:px-7 absolute box-border lg:py-16 lg:text-2xl lg:px-7 lg:h-auto lg:w-1/4">
+        <h1 className="border-b-4 border-pink-600 text-2xl w-min mb-3 mt-2 md:border-b-8 md:mb-12 md:text-5xl md:mt-0 lg:text-4xl lg:mb-12 px-1 py-0 font-bold">Login</h1>
         {children}
     </form>
   );
--- src/components/Forms/FormField.js
+++ src/components/Forms/FormField.js
@@ -3,7 +3,7 @@ function FormField({ type, id, fname, onChange, caption, value, error, ...attr})
     <section>
     <label >{caption}</label>
     <i className="fas fa-at"></i>
-    <input className="w-full mb-5 border-b-2 h-10 md:h-14 md:border-b-4 md:mb-10 text-base md:text-3xl border-pink-600 bg-transparent"
+    <input className="w-full mb-5 border-b-2 h-10 md:h-14 md:border-b-4 md:mb-10 text-base md:text-3xl lg:text-xl lg:border-b-2 lg:h-10 border-pink-600 bg-transparent"
         type={type}
         id={id}
         name={fname}
